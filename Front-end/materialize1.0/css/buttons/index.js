438d7a90a64ee996092a89237eeac915df90f676
Gustavo <gustavosoaresbt@hotmail.com>
2022-02-26 09:36:56 -0300

    Front-end atualizado 26/02/2022

---
+++ Front-end/materialize1.0/css/buttons/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instance = M.FloatingActionButton.init(elems, {
+        direction: 'top'
+    });
+});
+
