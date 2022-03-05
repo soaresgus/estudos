d5481d8fe62e9444ec5effd06d2926fad171be30
Gustavo <gustavosoaresbt@hotmail.com>
2022-03-05 11:32:04 -0300

    Começo do MVC no PHP

---
+++ Back-end/MVCPHP/App/Models/info.php
@@ -0,0 +1,15 @@
+<?php
+    namespace App\Models;
+
+    use NF\Model\Model;
+
+    class Info extends Model
+    {
+        public function getInfo()
+        {
+            $query = 'SELECT titulo, descricao FROM tb_info';
+
+            return $this->db->query($query)->fetchAll();
+        }
+    }
+?>
